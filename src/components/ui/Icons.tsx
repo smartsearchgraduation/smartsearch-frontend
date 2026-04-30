7a30ff1ef78697b860cd14333b6f3b77782c7496
Umut Şen <umutsen2662@gmail.com>
2026-04-30 16:00:57 +0300

    Add explicit width and height attributes to Search icon SVG to fix it not appearing on safari

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -50,6 +50,8 @@ export const Icons = {
     Search: (props: SVGProps<SVGSVGElement>) => (
         <svg
             xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
